3c990caebad6a8a3bd6cea3e931d6dc174b37077
Monesh K <62913922+coingyaan@users.noreply.github.com>
2026-05-09 04:07:35 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,77 +1,32 @@
-import { createConfig, http } from "wagmi";
-import { defineChain } from "viem";
+export const CONTRACT_ADDRESS = "0x0E6d86a33FF7fB39BBc3F512f8A804E10295939f";
 
-export const arcTestnet = defineChain({
+export const arcTestnet = {
   id: 5042002,
   name: "Arc Testnet",
-  nativeCurrency: {
-    name: "USDC",
-    symbol: "USDC",
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: { http: ["https://rpc.testnet.arc.network"] },
-  },
-  blockExplorers: {
-    default: { name: "ArcScan", url: "https://testnet.arcscan.app" },
-  },
-  testnet: true,
-});
-
-export const CONTRACT_ADDRESS = "0x5B538f515174a11b6c38B26d00dDeD5d6E99E6fC";
+  network: "arcTestnet",
+  nativeCurrency: { name: "USDC", symbol: "USDC", decimals: 18 },
+  rpcUrls: { default: { http: ["https://rpc.testnet.arc.network"] } },
+  blockExplorers: { default: { name: "ArcScan", url: "https://testnet.arcscan.app" } },
+};
 
 export const CONTRACT_ABI = [
-  {
-    inputs: [{ name: "referrer", type: "address" }],
-    name: "gm",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalGMs",
-    outputs: [{ name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ name: "wallet", type: "address" }],
-    name: "getUser",
-    outputs: [
-      { name: "_totalGMs", type: "uint256" },
-      { name: "_lastGMDay", type: "uint256" },
-      { name: "_streak", type: "uint256" },
-      { name: "_referredBy", type: "address" },
-      { name: "_referralCount", type: "uint256" },
-      { name: "_gmAvailableToday", type: "bool" },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getLeaderboard",
-    outputs: [
-      { name: "wallets", type: "address[]" },
-      { name: "gms", type: "uint256[]" },
-      { name: "streaks", type: "uint256[]" },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getUserCount",
-    outputs: [{ name: "", type: "uint256" }],
-    stateMutability: "view",
-    type: "function",
-  },
+  "function gm(address referrer) external",
+  "function restoreStreak() external",
+  "function getTotalGlobalGMs() view returns (uint256)",
+  "function getUserCount() view returns (uint256)",
+  "function getFirstGMToday() view returns (address)",
+  "function getUTCDay() view returns (uint256)",
+  "function getMultiplier(uint256 streak) view returns (uint256)",
+  "function getRestoreCost(address wallet) view returns (uint256)",
+  "function canRestore(address wallet) view returns (bool)",
+  "function getBadges(address w) view returns (uint256)",
+  "function hasBadge(address w, uint8 id) view returns (bool)",
+  "function getUserMain(address w) view returns (uint256 totalPoints, uint256 totalGMs, uint256 streak, bool canGMToday)",
+  "function getUserStreak(address w) view returns (uint256 streak, uint256 longestStreak, uint256 missedDay, bool canRestoreStreak, uint256 restoreCost)",
+  "function getUserReferral(address w) view returns (uint256 referralCount, address referredBy)",
+  "function getLeaderboardPage(uint256 start, uint256 end) view returns (address[] wallets, uint256[] points)",
+  "function getLeaderboardStats(uint256 start, uint256 end) view returns (uint256[] streaks, uint256[] gms, uint256[] badges)",
+  "function migrateUser(address wallet, uint256 totalPoints, uint256 totalGMs, uint256 streak, uint256 longestStreak) external",
+  "function adminUpdateUser(address wallet, uint256 totalPoints, uint256 totalGMs, uint256 streak, uint256 longestStreak) external",
+  "function transferOwnership(address newOwner) external",
 ];
-
-export const wagmiConfig = createConfig({
-  chains: [arcTestnet],
-  transports: {
-    [arcTestnet.id]: http(),
-  },
-});
